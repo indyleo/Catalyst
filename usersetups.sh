f9805caca16b4e2c100ad468cd9b806459c7e7f0
Indy <Indy@debian.org>
2025-03-15 14:09:24 -0400

    added figlet and a font

--- usersetups.sh
+++ usersetups.sh
@@ -121,7 +121,7 @@ echo "########################################"
 mkdir -pv ~/.config
 git_clone https://github.com/indyleo/Wallpapers.git ~/Pictures/Wallpapers/
 git_clone https://gitlab.com/dwt1/wallpapers.git ~/Pictures/wallpaper
-mv -v fastfetch git nvim lf tmux alacritty ohmyposh mimeapps.list user-dirs.locale user-dirs.dirs ~/.config/
+mv -v fastfetch git nvim lf tmux alacritty ohmyposh figletfonts mimeapps.list user-dirs.locale user-dirs.dirs ~/.config/
 rm -v ~/.bashrc ~/.profile ~/.zshenv ~/.zshrc
 mv -v .profile .zshenv .zshrc .functionrc .aliasrc .xsession .Xresources ~/
 
