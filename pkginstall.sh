8915745ceb737e9b21e872b51ca325ea932ea23d
Indy <Indy@debian.org>
2025-03-20 16:46:37 -0400

    made brave flatpak not a deb anymore

--- pkginstall.sh
+++ pkginstall.sh
@@ -42,13 +42,6 @@ apt install -y \
 
 systemctl enable xrdp
 
-# Install brave browser
-curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main" | \
-    tee /etc/apt/sources.list.d/brave-browser-release.list
-apt update
-apt install brave-browser
-
 echo "Packages installed successfully"
 
 echo "Now setting up Sddm theme"
--- usersetups.sh
+++ usersetups.sh
@@ -181,7 +181,7 @@ echo "## Flatpak Install ##"
 echo "#####################"
 flatpak install -y io.github.dweymouth.supersonic com.obsproject.Studio io.github.arunsivaramanneo.GPUViewer org.fedoraproject.MediaWriter com.chatterino.chatterino net.lutris.Lutris \
     com.github.tchx84.Flatseal org.prismlauncher.PrismLauncher com.heroicgameslauncher.hgl xyz.xclicker.xclicker dev.vencord.Vesktop org.winehq.Wine com.usebottles.bottles \
-    com.valvesoftware.steam net.davidotek.pupgui2 org.fkoehler.KTailctl
+    com.valvesoftware.steam net.davidotek.pupgui2 org.fkoehler.KTailctl com.brave.Browser
 
 flatpak install --user https://sober.vinegarhq.org/sober.flatpakref
 
