72177a1d214fcad7604bfbb1aa36605213a60381
Indy <Indy@debian.org>
2025-04-25 22:38:53 -0400

    removing kwin better blur

--- compile.sh
+++ compile.sh
@@ -13,7 +13,6 @@ echo "Cloning repositories..."
 git_clone https://github.com/bayasdev/envycontrol.git ~/Github/envycontrol
 git_clone https://github.com/indyleo/scripts.git ~/.local/scripts
 git_clone https://github.com/jesseduffield/lazygit.git ~/Github/lazygit
-git_clone https://github.com/taj-ny/kwin-effects-forceblur.git ~/Github/kwin-effects-forceblur
 git_clone https://github.com/tsujan/Kvantum.git ~/Github/Kvantum
 git_clone https://codeberg.org/AnErrupTion/ly.git ~/Github/ly
 
@@ -30,20 +29,6 @@ sudo luarocks install luacheck
 
 echo "Installing spotdl..."
 pipx install spotdl
-
-echo "Installing kwin-effects-forceblur..."
-cd ~/Github/kwin-effects-forceblur
-if command -v kwin_x11 &> /dev/null; then
-    mkdir build
-    cd build
-    cmake ../ -DCMAKE_INSTALL_PREFIX=/usr
-    make -j
-    sudo make install
-    cd "$builddir"
-else
-    echo "Skipping kwin-effects-forceblur, kwin_x11 not found"
-    cd "$builddir"
-fi
 
 echo "Installing kvantum..."
 cd ~/Github/Kvantum/Kvantum
