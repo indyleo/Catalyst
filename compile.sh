1b2c4d95bf346251a75f9ee090eede398a6f1c12
Indy <Indy@debian.org>
2025-10-18 01:40:38 -0400

    protonup

--- compile.sh
+++ compile.sh
@@ -36,6 +36,9 @@ sudo luarocks install luacheck
 echo "Installing spotdl..."
 pipx install spotdl
 
+echo "Installing protonup..."
+pipx install protonup
+
 echo "Installing bitwarden cli..."
 sudo npm install -g @bitwarden/cli
 
