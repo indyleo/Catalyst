6e0576dc525577e0062d7916224d626d26a780c8
Indy <Indy@debian.org>
2025-12-02 22:02:49 -0500

    feat: fixed icons

--- themer.sh
+++ themer.sh
@@ -5,11 +5,16 @@ echo "Downloading Wallpapers..."
 git_clone https://github.com/indyleo/Wallpapers.git ~/Pictures/Wallpapers/
 
 echo "Cursor Theme's"
-mkdir -vp ~/.local/share/icons
+mkdir -vp ~/.local/share/icons ~/.icons
 namer_capitaine=$(curl -s https://api.github.com/repos/sainnhe/capitaine-cursors/releases/latest | jq -r .name)
 wget "https://github.com/sainnhe/capitaine-cursors/releases/download/${namer_capitaine}/Linux.zip" -O Cursors.zip
 unzip Cursors.zip -d ~/.local/share/icons
 rm -fv Cursors.zip
+cd "$ORIGINAL_DIR"
+ln -s ~/.local/share/icons/default ~/.icons/
+ln -s ~/.local/share/icons/Capitaine\ Cursors\ \(Nord\) ~/.icons/
+ln -s ~/.local/share/icons/Capitaine\ Cursors\ \(Gruvbox\) ~/.icons/
+
 
 echo "Nord Icon Theme"
 wget -qO- https://git.io/papirus-icon-theme-install | DESTDIR="$HOME/.local/share/icons" sh
