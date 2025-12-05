27f69115c1a6d1923d9d3d8c171b1bd21e7a959b
Indy <Indy@debian.org>
2025-12-05 10:45:03 -0500

    feat: better zsh vi mode

--- zsh-plugins.sh
+++ zsh-plugins.sh
@@ -7,5 +7,6 @@ git_clone https://github.com/zsh-users/zsh-history-substring-search.git "${PLUGI
 git_clone https://github.com/zsh-users/zsh-completions.git "${PLUGINS_DIR}"/zsh-completions
 git_clone https://github.com/MichaelAquilina/zsh-you-should-use.git "${PLUGINS_DIR}"/zsh-you-should-use
 git_clone https://github.com/hlissner/zsh-autopair.git "${PLUGINS_DIR}"/zsh-autopair
+git_clone https://github.com/jeffreytse/zsh-vi-mode.git "${PLUGINS_DIR}"/zsh-vi-mode
 
 echo "Done"
