3ac2f24f25c75aaf13a2782ef1b03ba8036ae196
Indy <Indy@debian.org>
2025-12-26 00:07:20 -0500

    zsh

--- zsh-plugins.sh
+++ zsh-plugins.sh
@@ -3,6 +3,8 @@
 PLUGINS_DIR="${XDG_DATA_HOME:-$HOME/.local/share}/zplugins"
 
 echo "Cloning zsh plugins"
+git_clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${PLUGINS_DIR}"/zsh-syntax-highlighting
+git_clone https://github.com/zsh-users/zsh-autosuggestions.git "${PLUGINS_DIR}"/zsh-autosuggestions
 git_clone https://github.com/zsh-users/zsh-history-substring-search.git "${PLUGINS_DIR}"/zsh-history-substring-search
 git_clone https://github.com/zsh-users/zsh-completions.git "${PLUGINS_DIR}"/zsh-completions
 git_clone https://github.com/MichaelAquilina/zsh-you-should-use.git "${PLUGINS_DIR}"/zsh-you-should-use
