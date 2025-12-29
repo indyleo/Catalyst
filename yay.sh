7449be600a2fafb4855c8114fabc4f424728b01c
Indy <Indy@debian.org>
2025-12-29 14:26:28 -0500

    fdsafsd

--- yay.sh
+++ yay.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 builddir=$(pwd)
 
-cd ~
-sudo pacman -S --needed git base-devel
-git clone https://aur.archlinux.org/yay-bin.git
-cd yay-bin
-makepkg -si
-cd "$builddir"
+if ! command -v yay &> /dev/null; then
+    cd ~
+    sudo pacman -S --needed git base-devel
+    git clone https://aur.archlinux.org/yay-bin.git
+    cd yay-bin
+    makepkg -si
+    cd "$builddir"
+else
+    echo "Yay is already installed"
+fi
+
