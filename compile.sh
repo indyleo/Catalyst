b1f0bde6c8eaf9021d4ecb56b17a7cf4e1679eb2
Indy <Indy@debian.org>
2026-01-01 20:34:09 -0500

    rm subsonic

--- compile.sh
+++ compile.sh
@@ -4,7 +4,6 @@ builddir=$(pwd)
 
 echo "Cloning repositories..."
 git_clone https://github.com/indyleo/scripts.git ~/.local/scripts
-git_clone https://git.dayanhub.com/sagi/subsonic-tui.git ~/Github/subsonic-tui
 
 echo "Installing lua linter..."
 sudo luarocks install luacheck
@@ -14,13 +13,3 @@ pipx install spotdl
 
 echo "Installing protonup..."
 pipx install protonup
-
-echo "Installing subsonic-tui..."
-if ! command -v subsonic-tui &> /dev/null; then
-    cd ~/Github/subsonic-tui
-    make build
-    make install
-    cd "$builddir"
-else
-    echo "subsonic-tui is already installed"
-fi
