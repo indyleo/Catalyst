07feae5058155ff5f060a1ebc9afe3e822fca401
Indy <Indy@debian.org>
2026-01-09 13:44:24 -0500

    themes

--- themer.sh
+++ themer.sh
@@ -4,30 +4,14 @@ ORIGINAL_DIR=$(pwd)
 echo "Downloading Wallpapers..."
 git_clone https://github.com/indyleo/Wallpapers.git ~/Pictures/Wallpapers/
 
-echo "Cursor Theme's"
-mkdir -vp ~/.local/share/icons ~/.icons
-namer_capitaine=$(curl -s https://api.github.com/repos/sainnhe/capitaine-cursors/releases/latest | jq -r .name)
-wget "https://github.com/sainnhe/capitaine-cursors/releases/download/${namer_capitaine}/Linux.zip" -O Cursors.zip
-unzip Cursors.zip -d ~/.local/share/icons
-rm -fv Cursors.zip
-cd "$ORIGINAL_DIR"
-
-echo "Nord Icon Theme"
-wget -qO- https://git.io/papirus-icon-theme-install | DESTDIR="$HOME/.local/share/icons" sh
-
-echo "Setting up Nord GTK Theme"
+echo "Setting up Calamity	GTK Theme & Icon Theme..."
 mkdir -vp ~/.local/share/themes
-tag_nord=$(git ls-remote --tags https://github.com/EliverLara/Nordic.git | grep -o 'refs/tags/.*' | sed 's/refs\/tags\///' | grep -v '{}' | sort -V | tail -n 1)
-wget "https://github.com/EliverLara/Nordic/releases/download/${tag_nord}/Nordic.tar.xz" -O Nordic.tar.xz
-tar xf Nordic.tar.xz -C ~/.local/share/themes/
-rm -fv Nordic.tar.xz
-
-echo "Setting up Nord Kvantum Theme"
-sudo mkdir -vp /usr/share/Kvantum
-git_clone https://github.com/tonyfettes/materia-nord-kvantum.git materia-nord-kvantum
-cd materia-nord-kvantum
-sudo cp -vr Kvantum/MateriaNordDark /usr/share/Kvantum
-cd "$ORIGINAL_DIR"
-rm -rfv materia-nord-kvantum
+wget "https://github.com/indyleo/ClamityGTK/raw/refs/heads/main/CalamityTheme.tar.gz" -O CalamityTheme.tar.gz
+tar xf CalamityTheme.tar.gz -C ~/.local/share/themes/
+rm -fv CalamityTheme.tar.gz
+mkdir -vp ~/.local/share/icons
+wget "https://github.com/indyleo/ClamityGTK/raw/refs/heads/main/CalamityIcons.tar.gz" -O CalamityIcons.tar.gz
+tar xf CalamityIcons.tar.gz -C ~/.local/share/icons/
+rm -fv CalamityIcons.tar.gz
 
 echo "Done"
