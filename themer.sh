ccab4c0a3b3abdde4a39d09a99923f74056d3959
Indy <Indy@debian.org>
2026-03-31 19:21:36 -0400

    update'

--- themer.sh
+++ themer.sh
@@ -4,14 +4,11 @@ ORIGINAL_DIR=$(pwd)
 echo "Downloading Wallpapers..."
 git_clone https://github.com/indyleo/Wallpapers.git ~/Pictures/Wallpapers/
 
-echo "Setting up Calamity	GTK Theme & Icon Theme..."
-mkdir -vp ~/.local/share/themes
-wget "https://github.com/indyleo/ClamityGTK/raw/refs/heads/main/CalamityTheme.tar.gz" -O CalamityTheme.tar.gz
-tar xf CalamityTheme.tar.gz -C ~/.local/share/themes/
-rm -fv CalamityTheme.tar.gz
-mkdir -vp ~/.local/share/icons
-wget "https://github.com/indyleo/ClamityGTK/raw/refs/heads/main/CalamityIcons.tar.gz" -O CalamityIcons.tar.gz
-tar xf CalamityIcons.tar.gz -C ~/.local/share/icons/
-rm -fv CalamityIcons.tar.gz
+echo "Gruvbox Kvantum Themes..."
+cd ~/Github
+git_clone https://github.com/sachnr/gruvbox-kvantum-themes.git
+cd gruvbox-kvantum-themes
+sudo mv -v Gruvbox* /usr/share/Kvantum/
+cd "$ORIGINAL_DIR"
 
 echo "Done"
