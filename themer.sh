f06ed43a1a3592c8bb60ee1b5256bf8e6597744b
Indy <Indy@debian.org>
2026-07-27 13:12:55 -0400

    there

--- themer.sh
+++ themer.sh
@@ -6,7 +6,7 @@ git_clone https://github.com/indyleo/Wallpapers.git ~/Pictures/Wallpapers/
 
 echo "Gruvbox Kvantum Themes..."
 cd ~/Github
-git_clone https://github.com/sachnr/gruvbox-kvantum-themes.git
+git_clone https://github.com/sachnr/gruvbox-kvantum-themes.git gruvbox-kvantum-themes
 cd gruvbox-kvantum-themes
 sudo mv -v Gruvbox* /usr/share/Kvantum/
 cd "$ORIGINAL_DIR"
