3ee7d1b83f607d72e47fbf85bdecb629f9c6efa2
Indy <Indy@debian.org>
2026-08-10 16:16:36 -0400

    new sunshine/ufw stuff

--- ufw.sh
+++ ufw.sh
@@ -6,4 +6,9 @@ sudo ufw allow 443/tcp
 sudo ufw allow 3389/tcp
 sudo ufw default deny incoming
 sudo ufw default allow outgoing
+
+sudo mv -v sunshine /etc/ufw/applications.d/sunshine
+sudo ufw app update Sunshine
+sudo ufw allow Sunshine
+
 sudo ufw enable
